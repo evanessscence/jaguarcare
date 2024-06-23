82e82b33710ef779d5ed8eeb9bd1bd5c70b690b4
evanessscence <acbellorin@uamv.edu.ni>
2024-06-22 18:37:27 -0600

    Nombre del proyecto en la consola al iniciar el programa

--- src/main/java/uam/fia/jaguarcare/JaguarcareApplication.java
+++ src/main/java/uam/fia/jaguarcare/JaguarcareApplication.java
@@ -7,6 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JaguarcareApplication {
 
 	public static void main(String[] args) {
+		String applicationTitle = "Universidad Americana";
+		String applicationVersion = "1.0.0";
+
+		String asciiArt =
+				"      ,--.                                                                  \n" +
+						"     |  | ,--,--. ,---.,--.,--. ,--,--.,--.--. ,---. ,--,--.,--.--. ,---.  \n" +
+						" ,--. |  |' ,-.  || .-. |  ||  |' ,-.  ||  .--'| .--'' ,-.  ||  .--'| .-. : \n" +
+						" |  '-'  /\\ '-'  |' '-' '  ''  '\\ '-'  ||  |   \\ `--.\\ '-'  ||  |   \\   --. \n" +
+						"  `-----'  `--`--'.`-  / `----'  `--`--'`--'    `---' `--`--'`--'    `----' \n" +
+						"                  `---'                                                      \n" +
+						"${application.title} ${application.version}\n" +
+						"Powered by Spring Boot";
+
+		String display = asciiArt
+				.replace("${application.title}", applicationTitle)
+				.replace("${application.version}", applicationVersion);
+
+		System.out.println(display);
+
 		SpringApplication.run(JaguarcareApplication.class, args);
 	}
 
